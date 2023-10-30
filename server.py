14fecff3deffb1909b55d77e12ab12fcb5524b60
TejasChalke <tejaschalke66@gmail.com>
2023-10-30 10:33:35 +0530

    changed the endpoint from post to get

--- server.py
+++ server.py
@@ -1,23 +1,25 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import joblib
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 app = Flask(__name__)
+CORS(app)
 
 # Load the saved model
 model = joblib.load('svm_sentiment_model.pkl')
 
 vectorizer = joblib.load('svm_tfidf_vectorizer.pkl')
 
-@app.route('/predict_sentiment', methods=['POST'])
+@app.route('/predict_sentiment', methods=['GET'])
 def predict_sentiment():
-    data = request.get_json()
-    text = data['text']
+    text = request.args.get('text')
 
     text_vectorized = vectorizer.transform([text])
     sentiment = model.predict(text_vectorized)[0]
     sentiment = int(sentiment)
     
+    # 0: bad, 1: good
     return jsonify({'sentiment': sentiment})
 
 if __name__ == '__main__':
